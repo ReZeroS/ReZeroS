673b39d077eb3a1faef5624666d0d6457b71fa4e
ReZero <ljrezero@gmail.com>
2020-11-02 16:01:42 +0800

    Update update_readme.js

--- update_readme.js
+++ update_readme.js
@@ -15,21 +15,21 @@ function renderREADME(followersUrl, totalFollowers, selectedFollowers) {
   
   ![ReZero's github stats](https://github-readme-stats.vercel.app/api?username=rezeros&show_icons=true&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515)
   
-  <p>
+  
+  <a href="https://github.com/rezeros">
+    <img align="left" src="https://github-readme-stats.vercel.app/api/top-langs/?username=rezeros&layout=compact" />
+  </a>
   <a href="https://github.com/rezeros/zit">
     <img align="left" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=zit&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
   </a>
-  </p>
-  <p>
   <a href="https://github.com/rezeros/zerobox">
     <img align="left" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=zerobox&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
   </a>
-  </p>
-  <p>  
   <a href="https://github.com/rezeros/leetcode">
     <img align="left" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=leetcode&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
   </a>
-  </p>
+ 
+  
 
     ## My Followers ([${totalFollowers}](${followersUrl}))
     
