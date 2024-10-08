db04c89d5aa555aecd6f11e1de1802725d39cdcd
ReZero <1726542850@qq.com>
2024-10-08 17:28:46 +0800

    Update update_readme.js

--- update_readme.js
+++ update_readme.js
@@ -7,7 +7,7 @@ function renderREADME(followersUrl, totalFollowers, selectedFollowers) {
 
   I am a developer based in China/Shanghai.
   
-  - 📫 [Blog] https://qqtim.club
+  - 📫 [Blog] https://rezeros.github.io
 
   <a href="https://github.com/rezeros/git">
     <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=git&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
