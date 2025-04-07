86995415fb7f56129f04f88deeb6967c3dfe0a49
ReZero <1726542850@qq.com>
2025-04-07 11:06:52 +0800

    remove duplicate pin info

--- update_readme.js
+++ update_readme.js
@@ -7,20 +7,6 @@ function renderREADME() {
   I am a developer based in China/Shanghai.
   
   - 📫 [Blog] https://rezeros.github.io
-  
-   <a href="https://github.com/rezeros/Jaxer">
-    <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=Jaxer&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
-  </a>
-  <a href="https://github.com/rezeros/git">
-    <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=git&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
-  </a>
-  <a href="https://github.com/rezeros/zerobox">
-    <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=zerobox&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
-  </a>
-
-  <a href="https://github.com/rezeros/leetcode">
-    <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=rezeros&repo=leetcode&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515" />
-  </a>
 `;
 
   return Buffer.from(readme.replace(/^ {4}/gm, '')).toString('base64');
